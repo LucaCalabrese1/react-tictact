dc8549ee76400afa75d4b9bfe91563db216be95f
Luca <l.calabrese@educasoftware.it>
2024-03-20 16:52:28 +0100

    player component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from './components/Player.jsx';
 
 function App() {
-  
-
   return (
-   <></>
-  )
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player name='Giocatore 1' symbol='X' />
+          <Player name='Giocatore 2' symbol='O' />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
